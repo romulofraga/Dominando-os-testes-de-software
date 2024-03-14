adc347e0d89ace96a642b98b2c83658d23797066
Rômulo Fraga <romulo.fraga@vtex.com>
2024-03-13 23:19:15 -0300

    Add Domain objects

---
+++ 02 - TDD/src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace NerdStore.Core;
+
+public abstract class Entity
+{
+  public Guid Id { get; set; }
+  public DateTime Timestamp { get; private set; }
+
+  protected Entity()
+  {
+    Id = Guid.NewGuid();
+    Timestamp = DateTime.Now;
+  }
+}
---
+++ 02 - TDD/src/NerdStore.Core/Messages/Event.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using NerdStore.Core.Messages;
+
+namespace NerdStore.Core;
+
+public abstract class Event : Message, INotification
+{
+  public DateTime Timestamp { get; private set; }
+
+  protected Event()
+  {
+    Timestamp = DateTime.Now;
+  }
+}
